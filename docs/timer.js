9320afd2efb2b675cee8593812d064b844dcdf9c
Siru-s <71216171+Siru-s@users.noreply.github.com>
2021-07-17 19:03:26 +0900

    Add files via upload

---
+++ docs/timer.js
@@ -0,0 +1,27 @@
+var dday = new Date("November 21, 2021, 2:00:00").getTime();
+
+setInterval(function() {
+  var today = new Date().getTime();
+  var gap = dday - today;
+  var day = Math.ceil(gap / (1000 * 60 * 60 * 24));
+  var hour = Math.ceil((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var min = Math.ceil((gap % (1000 * 60 * 60)) / (1000 * 60));
+  var sec = Math.ceil((gap % (1000 * 60)) / 1000);
+
+  document.getElementById("D1").innerHTML = day + "일";
+  document.getElementById("count1").innerHTML = hour + "시간 " + min + "분 " + sec + "초";
+}, 1000);
+
+var dday1 = new Date("August 24, 2021, 2:00:00").getTime();
+
+setInterval(function() {
+  var today1 = new Date().getTime();
+  var gap1 = dday1 - today1;
+  var day1 = Math.ceil(gap1 / (1000 * 60 * 60 * 24));
+  var hour1 = Math.ceil((gap1 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var min1 = Math.ceil((gap1 % (1000 * 60 * 60)) / (1000 * 60));
+  var sec1 = Math.ceil((gap1 % (1000 * 60)) / 1000);
+
+  document.getElementById("D2").innerHTML = day1 + "일";
+  document.getElementById("count2").innerHTML = hour1 + "시간 " + min1 + "분 " + sec1 + "초";
+}, 1000);	
